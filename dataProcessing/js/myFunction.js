4277a4a8bb3dcbae78187dc01e186778f59c4f3c
Korney-EV <korney-ev@yandex.ru>
2022-09-30 16:34:39 +0300

    2

--- dataProcessing/js/myFunction.js
+++ dataProcessing/js/myFunction.js
@@ -43,4 +43,4 @@ var spans = document.getElementsByClassName("simple-text")
 for (var i = 0; i < spans.length; i++) {
     console.log(spans[i].innerHTML);
 }
-//Выбор элемента по названию class
+//Выбор элемента по названию class222
