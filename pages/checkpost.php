299b04d51d89b667526ef7a640282bbc6575d315
Korney-EV <korney-ev@yandex.ru>
2022-10-04 00:59:57 +0300

    6-Подключение и обработка файла JSON

--- pages/checkpost.php
+++ pages/checkpost.php
@@ -10,5 +10,31 @@
     $jsonStr = json_encode($jsonData);
     var_dump($jsonStr);
 
+    //Чтение JSON файлов
+//    $res = file_get_contents("doc/bd.json");
+    //Преоброзование строки в массив
+//    $bdData = json_decode($res, true);
+    //Внесение изменений в json файл
+//    for ($i = 0; $i < count($bdData); $i++) {
+//        $bdData[$i]["age"] = $bdData[$i]["age"] +10;
+//    }
+    /*
+    Запись данных в json файл
+    1. Преобровать массив в json строку - переменная = json_encode($jsonData);
+    2. Записать в файл file_put_contents(перобразованный в оыщт формат массив)
+    */
+    $file = file_get_contents('doc/bd.json');  // Открыть файл bd.json
+    $taskList = json_decode($file,TRUE);        // Декодировать в массив
+    unset($file);                               // Очистить переменную $file
+    $taskList[] = $jsonData;        // Представить новую переменную как элемент массива, в формате 'ключ'=>'имя переменной'
+    file_put_contents('doc/bd.json',json_encode($taskList));  // Перекодировать в формат и записать в файл.
+    unset($taskList);
+
+//    file_put_contents("doc/bd.json", $jsonStr);
+    print_r($jsonStr);
+    var_dump($jsonStr);
+    echo $jsonStr;
+
+
 
 
